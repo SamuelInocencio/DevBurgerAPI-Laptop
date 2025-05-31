39ebb7de6a26ddfa76c4b43b4110005b8f607758
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-05-31 12:54:56 -0300

    Change made in ProductController

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,13 +1,14 @@
 import * as Yup from 'yup';
 
 import Product from '../models/Product';
+import Category from '../models/Category';
 
 class ProductController {
   async store(request, response) {
     const schema = Yup.object({
       name: Yup.string().required(),
       price: Yup.number().required(),
-      category: Yup.string().required(),
+      category_id: Yup.number().required(),
     });
 
     try {
@@ -17,12 +18,12 @@ class ProductController {
     }
 
     const { filename: path } = request.file;
-    const { name, price, category, offer } = request.body;
+    const { name, price, category_id, offer } = request.body;
 
     const product = await Product.create({
       name,
       price,
-      category,
+      category_id,
       path,
     });
 
@@ -30,9 +31,17 @@ class ProductController {
   }
 
   async index(request, response) {
-    const products = await Product.findAll({});
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          as: 'category',
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
 
-    console.log({userId: request.userId})
+    console.log({ userId: request.userId });
 
     return response.json(products);
   }
