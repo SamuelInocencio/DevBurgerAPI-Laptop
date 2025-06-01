f572f2480a1b596e55c8193bd7284be81ddc2ee1
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-06-01 09:49:37 -0300

    implemented mongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import configDatabase from '../config/database';
 
@@ -6,13 +7,14 @@ import Category from '../app/models/Category';
 import Product from '../app/models/Product';
 import User from '../app/models/User';
 
+
 const models = [User, Product, Category];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
-
   init() {
     this.connection = new Sequelize(configDatabase);
     models
@@ -21,6 +23,12 @@ class Database {
         (model) => model.associate && model.associate(this.connection.models),
       );
   }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburger',
+    );
+  }
 }
 
 export default new Database();
