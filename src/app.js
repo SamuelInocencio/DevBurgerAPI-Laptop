e1000ee3fa32d1bc5d6884e395504b0ff0c75bcd
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-06-29 14:00:17 -0300

    updating API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import { resolve } from 'node:path';
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 import './database';
 
 class App {
   constructor() {
     this.app = express();
+    this.app.use(cors());
     this.middlewares();
     this.routes();
   }
